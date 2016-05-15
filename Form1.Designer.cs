6b887ca6443f1f546d872d2b874011e1b75274e2
Iván Bravo Bravo <kolesias17@gmail.com>
2016-05-14 23:47:49 -0500

    * First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SpeechDota
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose( bool disposing ) {
+			if ( disposing && (components != null) ) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent() {
+			this.enableBtn = new System.Windows.Forms.Button();
+			this.logBox = new System.Windows.Forms.TextBox();
+			this.quickCast = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// enableBtn
+			// 
+			this.enableBtn.Location = new System.Drawing.Point(52, 226);
+			this.enableBtn.Name = "enableBtn";
+			this.enableBtn.Size = new System.Drawing.Size(107, 23);
+			this.enableBtn.TabIndex = 0;
+			this.enableBtn.Text = "Comenzar";
+			this.enableBtn.UseVisualStyleBackColor = true;
+			this.enableBtn.Click += new System.EventHandler(this.enableBtn_Click);
+			// 
+			// logBox
+			// 
+			this.logBox.Location = new System.Drawing.Point(12, 12);
+			this.logBox.Multiline = true;
+			this.logBox.Name = "logBox";
+			this.logBox.Size = new System.Drawing.Size(260, 208);
+			this.logBox.TabIndex = 1;
+			this.logBox.Visible = false;
+			// 
+			// quickCast
+			// 
+			this.quickCast.AutoSize = true;
+			this.quickCast.Location = new System.Drawing.Point(194, 230);
+			this.quickCast.Name = "quickCast";
+			this.quickCast.Size = new System.Drawing.Size(74, 17);
+			this.quickCast.TabIndex = 2;
+			this.quickCast.Text = "Quickcast";
+			this.quickCast.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.quickCast);
+			this.Controls.Add(this.logBox);
+			this.Controls.Add(this.enableBtn);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button enableBtn;
+		private System.Windows.Forms.TextBox logBox;
+		private System.Windows.Forms.CheckBox quickCast;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Globalization;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SpeechDota
+{
+	public partial class Form1 : Form
+	{
+		Choices commands;
+		Grammar grammar;
+
+		SpeechRecognitionEngine recognizer;
+		GrammarBuilder gBuilder	= new GrammarBuilder();
+
+		[DllImport( "user32.dll" )]
+		public static extern int SetForegroundWindow( IntPtr hWnd );
+
+		[DllImport( "user32.dll" )]
+		public static extern bool PostMessage( IntPtr hWnd, UInt32 Msg, int wParam, int lParam );
+
+		[DllImport( "user32.dll" )]
+		public static extern bool SendMessage( IntPtr hWnd, UInt32 Msg, int wParam, int lParam );
+
+		[DllImport( "user32.dll" )]
+		private static extern short VkKeyScan( char ch );
+
+		const UInt32 WM_KEYDOWN = 0x0100;
+		const UInt32 WM_KEYUP = 0x0101;
+		const UInt32 WM_CHAR = 0x0102;
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load( object sender, EventArgs e ) {
+			// Creamos el motor de reconocimiento
+			recognizer = new SpeechRecognitionEngine( CultureInfo.InstalledUICulture );
+
+			// Comandos válidos
+			commands = new Choices();
+			commands.Add( "congelación" );
+			commands.Add( "congela" );
+			//commands.Add( "stun" );
+			commands.Add( "fantasma" );
+			commands.Add( "muro" );
+			commands.Add( "muro de hielo" );
+			commands.Add( "pem" );
+			commands.Add( "tornado" );
+			commands.Add( "presteza" );
+			commands.Add( "impacto" );
+			commands.Add( "espíritu" );
+			commands.Add( "meteoro" );
+			commands.Add( "metorito" );
+			commands.Add( "explosión" );
+
+			// Creamos el dirección de gramatica
+			gBuilder.Append( commands );
+			gBuilder.Culture = CultureInfo.InstalledUICulture;
+
+			grammar = new Grammar( gBuilder );
+
+			// Lo usamos
+			recognizer.LoadGrammar( grammar );
+			recognizer.SetInputToDefaultAudioDevice();
+
+			recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>( onSpeech );
+		}
+
+		/// <summary>
+		/// Envía un comando a la VConsole
+		/// </summary>
+		/// <param name="command">Comando</param>
+		public void sendVConsole( string command ) {
+			try {
+				// Buscamos el proceso del vConsole2
+				Process vConsole = Process.GetProcessesByName( "vconsole2" )[0];
+				Process dota = Process.GetProcessesByName( "DOTA2" )[0];
+
+				// Mandamos cada letra a la ventana de vconsole
+				foreach ( char c in command ) {
+					SetForegroundWindow( vConsole.MainWindowHandle );
+					SendMessage( vConsole.MainWindowHandle, WM_CHAR, (int)c, 0 );
+				}
+
+				// Enter!
+				SendMessage( vConsole.MainWindowHandle, WM_KEYDOWN, 13, 0 );
+				SetForegroundWindow( dota.MainWindowHandle );
+
+				// Quickcast
+				// Debemos esperar 60ms para que la habilidad se ponga en el slot 3
+				if ( quickCast.Checked && command != "dota_ability_quickcast 3" ) {
+					Thread.Sleep( 60 );
+					sendVConsole( "dota_ability_quickcast 3" );
+				}
+			}
+			catch( Exception e ) {
+				MessageBox.Show( "No se ha iniciado vconsole2.exe o dota2.exe", "Error fatal", MessageBoxButtons.OK, MessageBoxIcon.Error );
+				Application.Exit();
+			}
+		}
+
+		public void onSpeech( object sender, SpeechRecognizedEventArgs e ) {
+			switch ( e.Result.Text ) {
+				case "congelación":
+				case "congela":
+				case "stun":
+					sendVConsole( "dota_ability_quickcast 0; dota_ability_quickcast 0; dota_ability_quickcast 0; dota_ability_quickcast 5" );
+					break;
+
+				case "fantasma":
+					sendVConsole( "dota_ability_quickcast 0; dota_ability_quickcast 0; dota_ability_quickcast 1; dota_ability_quickcast 5" );
+					break;
+
+				case "muro":
+				case "muro de hielo":
+					sendVConsole( "dota_ability_quickcast 0; dota_ability_quickcast 0; dota_ability_quickcast 2; dota_ability_quickcast 5" );
+					break;
+
+				case "pem":
+					sendVConsole( "dota_ability_quickcast 1; dota_ability_quickcast 1; dota_ability_quickcast 1; dota_ability_quickcast 5" );
+					break;
+
+				case "tornado":
+					sendVConsole( "dota_ability_quickcast 1; dota_ability_quickcast 1; dota_ability_quickcast 0; dota_ability_quickcast 5" );
+					break;
+
+				case "presteza":
+					sendVConsole( "dota_ability_quickcast 1; dota_ability_quickcast 1; dota_ability_quickcast 2; dota_ability_quickcast 5" );
+					break;
+
+				case "impacto":
+					sendVConsole( "dota_ability_quickcast 2; dota_ability_quickcast 2; dota_ability_quickcast 2; dota_ability_quickcast 5" );
+					break;
+
+				case "espíritu":
+					sendVConsole( "dota_ability_quickcast 2; dota_ability_quickcast 2; dota_ability_quickcast 0; dota_ability_quickcast 5" );
+					break;
+
+				case "meteoro":
+				case "metorito":
+					sendVConsole( "dota_ability_quickcast 2; dota_ability_quickcast 2; dota_ability_quickcast 1; dota_ability_quickcast 5" );
+					break;
+
+				case "explosión":
+					sendVConsole( "dota_ability_quickcast 2; dota_ability_quickcast 2; dota_ability_quickcast 0; dota_ability_quickcast 5" );
+					break;
+
+				default:
+					MessageBox.Show( e.Result.Text, "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+					break;
+			}
+
+			logBox.Text = e.Result.Text + "\r\n" + logBox.Text;
+		}
+
+		private void enableBtn_Click( object sender, EventArgs e ) {
+			enableBtn.Enabled = false;
+			logBox.Visible = true;
+
+			// Ejecutamos invoker.cfg
+			sendVConsole( "exec invoker" );
+
+			recognizer.RecognizeAsync( RecognizeMode.Multiple );
+		}
+	}
+}
